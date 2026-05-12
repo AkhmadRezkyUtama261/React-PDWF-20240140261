935de1ecbeb26f33ae594c34ce13f6e5c05ce4ab
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2026-05-12 23:15:31 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from './pages/Login';
+// import Dashboard from './pages/Dashboard'; // Aktifkan ini jika ingin melihat dashboard
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
